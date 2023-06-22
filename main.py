1b1a66fda217bf8782386123860996e5f74a0ef1
scottw077 <scottw@stu.otc.school.nz>
2023-06-22 22:59:24 +0000

    started sign up page

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ from tkinter import *
 root = Tk()
 root.title("Electrical Job Management Software")
 root.iconbitmap("ElecTRICIAN JOB MANAGEMENT SOFTWARE (2).ico")
+root.geometry("1600x900")
+root.configure(bg="#5b5b5c")
 
 
+def sign_up():
+    signuptxt = Label(root, text="Sign Up")
+    signuptxt.grid(row=0, column=0)
+
+
+sign_up()
 root.mainloop()
