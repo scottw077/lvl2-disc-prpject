a0c5b9bf5f3c4a8dcd8baac135608d1e20808e48
scottw077 <scottw@stu.otc.school.nz>
2023-08-07 14:57:16 +1200

    dd

--- test.py
+++ test.py
@@ -1,38 +1,9 @@
 from tkinter import *
 from tkinter import ttk
+import bcrypt
 
-jobs_frame = Frame(root)
-jobs_frame.pack()
-
-my_game = ttk.Treeview(jobs_frame)
-
-my_game['columns'] = ('player_id', 'player_name',
-                      'player_Rank', 'player_states', 'player_city')
-
-my_game.column("#0", width=0,  stretch=NO)
-my_game.column("player_id", anchor=CENTER, width=10)
-my_game.column("player_name", anchor=CENTER, width=20)
-my_game.column("player_Rank", anchor=CENTER, width=30)
-my_game.column("player_states", anchor=CENTER, width=40)
-my_game.column("player_city", anchor=CENTER, width=50)
-
-my_game.heading("#0", text="", anchor=CENTER)
-my_game.heading("player_id", text="Id", anchor=CENTER)
-my_game.heading("player_name", text="Name", anchor=CENTER)
-my_game.heading("player_Rank", text="Rank", anchor=CENTER)
-my_game.heading("player_states", text="States", anchor=CENTER)
-my_game.heading("player_city", text="States", anchor=CENTER)
-
-my_game.insert(parent='', index='end', iid=100, text='',
-               values=['1', 'Ninja', '101', 'Oklahoma', 'Moore'])
-my_game.insert(parent='', index='end', iid=1, text='',
-               values=('2', 'Ranger', '102', 'Wisconsin', 'Green Bay'))
-my_game.insert(parent='', index='end', iid=2, text='',
-               values=('3', 'Deamon', '103', 'California', 'Placentia'))
-my_game.insert(parent='', index='end', iid=3, text='',
-               values=('4', 'Dragon', '104', 'New York', 'White Plains'))
-my_game.insert(parent='', index='end', iid=4, text='',
-               values=('5', 'CrissCross', '105', 'California', 'San Diego'))
-my_game.insert(parent='', index='end', iid=5, text='',
-               values=('6', 'ZaqueriBlack', '106', 'Wisconsin', 'TONY'))
-my_game.pack()
+# example password
+password = "password123"
+bytes = password.encode("utf-8")
+salt = bcrypt.gensalt()
+hash = bcrypt.hashpw(bytes, salt)
