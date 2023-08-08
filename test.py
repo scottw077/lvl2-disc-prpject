43c0acbe5a57f932b1bdcad06807555d72b4e528
scottw077 <scottw@stu.otc.school.nz>
2023-08-08 19:03:15 +1200

    hjbkl

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 from tkinter import *
 from tkinter import ttk
-import bcrypt
 
-# example password
-password = "password123"
-bytes = password.encode("utf-8")
-salt = bcrypt.gensalt()
-hash = bcrypt.hashpw(bytes, salt)
+
+wi = {"william": "dddd"}
+
+print(type(wi))
+
+
