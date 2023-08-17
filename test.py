af1859f253f6ee2ef1b766ff21450e52ea7804b2
scottw077 <scottw@stu.otc.school.nz>
2023-08-17 13:28:51 +1200

    123123123123213

--- test.py
+++ test.py
@@ -1,9 +1,18 @@
 from tkinter import *
 from tkinter import ttk
+import aspose.pdf as ap
+document = ap.Document()
 
+# Add page
+page = document.pages.add()
 
-wi = {"william": "dddd"}
+# Initialize textfragment object
+text_fragment = ap.text.TextFragment("Hello,world!")
+title = ap.text.TextFragment("test")
 
-print(type(wi))
+# Add text fragment to new page
+page.title.add(title)
+page.paragraphs.add(text_fragment)
 
-
+# Save updated PDF
+document.save("output.pdf")
