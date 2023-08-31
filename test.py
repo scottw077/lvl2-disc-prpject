78c392130657a26041fd1efa59d46275bc897867
scottw077 <scottw@stu.otc.school.nz>
2023-08-31 21:42:09 +1200

    asdasdasdasdasd

--- test.py
+++ test.py
@@ -1,18 +1,11 @@
 from tkinter import *
 from tkinter import ttk
-import aspose.pdf as ap
-document = ap.Document()
 
-# Add page
-page = document.pages.add()
+def invoice_create(desc, amount, price):
+    if (desc or amount or price) and not (desc and amount and price):
+        print("test", desc + amount)
 
-# Initialize textfragment object
-text_fragment = ap.text.TextFragment("Hello,world!")
-title = ap.text.TextFragment("test")
-
-# Add text fragment to new page
-page.title.add(title)
-page.paragraphs.add(text_fragment)
-
-# Save updated PDF
-document.save("output.pdf")
+invoice_create("", "", "")
+invoice_create("dd", "", "")
+invoice_create("dd", "a", "")
+invoice_create("asd", "asdas", "asda")
