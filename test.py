8b5c3a7cd6eb85b630c42f84d119fec844f1f760
scottw077 <scottw@stu.otc.school.nz>
2023-09-06 09:51:43 +1200

    pdf fule

--- test.py
+++ test.py
@@ -4,7 +4,8 @@ from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.lib import colors
 from reportlab.lib.units import inch
 
-def create_invoice(job_number, client_name, client_address, client_phone, date, items, subtotal, total, pdf_file):
+
+def create_invoice(job_number, client_name, client_address, client_phone, date, items, subtotal, total, pdf_file, gst):
     doc = SimpleDocTemplate(pdf_file, pagesize=letter)
 
     # Create a list to hold the invoice elements
@@ -16,19 +17,25 @@ def create_invoice(job_number, client_name, client_address, client_phone, date, 
     # Invoice header
     header = Paragraph("Invoice", styles['Heading1'])
     elements.append(header)
-    elements.append(Paragraph("Job Number: {}".format(job_number), styles['Normal']))
+    elements.append(
+        Paragraph("Job Number: {}".format(job_number), styles['Normal']))
     elements.append(Paragraph("Date: {}".format(date), styles['Normal']))
-    elements.append(Paragraph("Client Name: {}".format(client_name), styles['Normal']))
-    elements.append(Paragraph("Client Address: {}".format(client_address), styles['Normal']))
-    elements.append(Paragraph("Client Phone: {}".format(client_phone), styles['Normal']))
+    elements.append(
+        Paragraph("Client Name: {}".format(client_name), styles['Normal']))
+    elements.append(Paragraph("Client Address: {}".format(
+        client_address), styles['Normal']))
+    elements.append(Paragraph("Client Phone: {}".format(
+        client_phone), styles['Normal']))
 
     # Itemized list
-    data = [["Item", "Description", "Quantity", "Unit Price", "Total"]]
+    data = [["Description", "Quantity", "Unit Price", "GST", "Total"]]
     for item in items:
-        data.append([item["name"], item["description"], item["quantity"], item["unit_price"], item["total"]])
+        data.append([item["description"], item["quantity"],
+                    '${:.2f}'.format(item["unit_price"]), '${:.2f}'.format(item["GST"]), '${:.2f}'.format(item["total"])])
 
     # Create a table for the itemized list
-    table = Table(data, colWidths=[1.5*inch, 3*inch, 1*inch, 1.5*inch, 1.5*inch])
+    table = Table(data, colWidths=[4*inch, 1 *
+                  inch, 1*inch, 1*inch, 1.5*inch])
     table.setStyle(TableStyle([
         ('BACKGROUND', (0, 0), (-1, 0), colors.grey),
         ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
@@ -42,11 +49,16 @@ def create_invoice(job_number, client_name, client_address, client_phone, date, 
     elements.append(table)
 
     # Subtotal and Total
-    elements.append(Paragraph("Subtotal: ${:.2f}".format(subtotal), styles['Normal']))
-    elements.append(Paragraph("Total: ${:.2f}".format(total), styles['Normal']))
+    elements.append(
+        Paragraph("Subtotal: ${:.2f}".format(subtotal), styles['Heading4']))
+    elements.append(Paragraph("GST: ${:.2f}".format(gst), styles['Heading4']))
+
+    elements.append(
+        Paragraph("Total: ${:.2f}".format(total), styles['Heading2']))
 
     # Build the PDF
     doc.build(elements)
+
 
 if __name__ == "__main__":
     job_number = "12345"
@@ -55,12 +67,18 @@ if __name__ == "__main__":
     client_phone = "555-555-5555"
     date = "2023-09-05"
     items = [
-        {"name": "Item 1", "description": "Description 1", "quantity": 2, "unit_price": 50.00, "total": 100.00},
-        {"name": "Item 2", "description": "Description 2", "quantity": 3, "unit_price": 30.00, "total": 90.00}
+        {"description": "Description 1", "quantity": 2,
+            "unit_price": 50.00, "GST": 15.00, "total": 115.00},
+        {"description": "Description 2", "quantity": 3,
+            "unit_price": 30.00, "GST": 13.50, "total": 103.50}
     ]
-    subtotal = sum(item["total"] for item in items)
-    total = subtotal
+    subtotal = sum(item["quantity"] * item["unit_price"] for item in items)
 
-    pdf_file = "invoice_with_items.pdf"
+    gst = sum(item["GST"] for item in items)
 
-    create_invoice(job_number, client_name, client_address, client_phone, date, items, subtotal, total, pdf_file)
+    total = sum(item["total"] for item in items)
+
+    pdf_file = "invoice_with_items5.pdf"
+
+    create_invoice(job_number, client_name, client_address,
+                   client_phone, date, items, subtotal, total, pdf_file, gst)
